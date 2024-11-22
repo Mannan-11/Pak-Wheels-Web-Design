ca9fd6a3bf1360009abb5689883d05d1545dfd6a
Mannan-11 <mannansheikh40@gmail.com>
2024-11-23 04:23:11 +0500

    Third Commit

--- app/component/Footer/page.tsx
+++ app/component/Footer/page.tsx
@@ -6,7 +6,7 @@ export default function Footer(){
             <footer>
                 <nav className="flex flex-row text-black align-bottom border border-solid border-white justify-evenly bg-gray-200 w-full h-20 items-center ">
                     <Link href = "/" target = "_blank" className= "hover:font-bold">Home Page</Link>
-                    <Link href = "/" className= "hover:font-bold">Designed By: Abdul Mannan</Link>
+                    <Link href = "https://github.com/Mannan-11/Pak-Wheels-Web-Design" className= "hover:font-bold">Designed By: Abdul Mannan</Link>
                 </nav>
             </footer>
 
